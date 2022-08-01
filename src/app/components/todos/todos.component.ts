da96b0035456324251e8cbf2c56615d7701222d4
Tim <Timmykmcc@gmail.com>
2022-08-01 18:54:15 -0400

    adding styling and models for the list

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos!:Todo[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First Todo',
+        completed: false
+      },
+      {
+        content: 'Second Todo',
+        completed: false
+      }
+    ]
+  }
+
+  toggleDone(id: any) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+}
